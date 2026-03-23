af91d2c3c236304b214be958c01d95dd84ec910e
Laxminarasimha <singampallilaxminarasimha10@gmail.com>
2026-03-23 23:34:16 +0530

    update

--- DevOpsPracticeLab.java
+++ DevOpsPracticeLab.java
@@ -1,5 +1,5 @@
 public class DevOpsPracticeLab {
     public static void main(String[] args) {
-        System.out.println("hii this is devops practice lab");
+        System.out.println("hii this is devops faculty");
     }
 }
